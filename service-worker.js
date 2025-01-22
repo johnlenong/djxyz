8669cce53abb398b548e1c6fa504b805a9edf63e
johnlenong <johnlenong2020@gmail.com>
2025-01-22 10:31:21 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,62 +1,54 @@
 const CACHE_NAME = 'drivejav-cache-v1';
 const urlsToCache = [
-  '/', 
-  '/index.html', 
-  '/style.css', 
-  '/script.js', 
-  '/manifest.json', 
-  '/icon/android-chrome-192x192.png', 
-  '/icon/android-chrome-512x512.png' 
+	'/',
+	'/style.css',
+	'/script.js',
+	'/manifest.json',
+	'/icon/android-chrome-192x192.png',
+	'/icon/android-chrome-512x512.png'
 ];
 
 
 self.addEventListener('install', (event) => {
-  console.log('[Service Worker] Installing...');
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('[Service Worker] Caching app shell');
-      return cache.addAll(urlsToCache);
-    })
-  );
+	event.waitUntil(
+		caches.open(CACHE_NAME).then((cache) => {
+			return cache.addAll(urlsToCache);
+		})
+	);
 });
 
 
 self.addEventListener('activate', (event) => {
-  console.log('[Service Worker] Activating...');
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            console.log('[Service Worker] Deleting old cache:', cache);
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-  );
+	event.waitUntil(
+		caches.keys().then((cacheNames) => {
+			return Promise.all(
+				cacheNames.map((cache) => {
+					if (cache !== CACHE_NAME) {
+						return caches.delete(cache);
+					}
+				})
+			);
+		})
+	);
 });
 
 
 self.addEventListener('fetch', (event) => {
-  console.log('[Service Worker] Fetching:', event.request.url);
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      if (cachedResponse) {
-        console.log('[Service Worker] Returning cached response:', event.request.url);
-        return cachedResponse;
-      }
-      console.log('[Service Worker] Fetching from network:', event.request.url);
-      return fetch(event.request).then((networkResponse) => {
+	event.respondWith(
+		caches.match(event.request).then((cachedResponse) => {
+			if (cachedResponse) {
+				return cachedResponse;
+			}
+			return fetch(event.request).then((networkResponse) => {
 
-        return caches.open(CACHE_NAME).then((cache) => {
-          cache.put(event.request, networkResponse.clone());
-          return networkResponse;
-        });
-      });
-    }).catch(() => {
-      console.error('[Service Worker] Fetch failed; returning offline page');
-      return caches.match('/index.html'); 
-    })
-  );
+				return caches.open(CACHE_NAME).then((cache) => {
+					cache.put(event.request, networkResponse.clone());
+					return networkResponse;
+				});
+			});
+		}).catch(() => {
+			console.error('[Service Worker] Fetch failed; returning offline page');
+			return caches.match('/index.html');
+		})
+	);
 });
